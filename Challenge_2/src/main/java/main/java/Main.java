ef5ee9622bf4665db507c9c9fc360ac2cb522c3d
Brieuc <brieuc.olivier@student.unamur.be>
2018-04-28 16:33:57 +0200

    Initial Push
    Initial push fort Challenge_2 #preparezvous

---
+++ Challenge_2/src/main/java/main/java/Main.java
@@ -0,0 +1,20 @@
+package main.java;
+
+public class Main {
+
+    // URI les plus utilisés
+    static String rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns/";
+    static String rdfs = "https://www.w3.org/2000/01/rdf-schema/";
+    static String foaf = "http://xmlns.com/foaf/0.1/";
+
+    // Fichiers & Query
+    static String queryfile = "D:\\Dropbox\\2017-2018Master2\\Techno_Web\\TP\\InteliJ\\BeautyOne\\src\\main\\resources\\test.rq";
+    static String queryXML = "D:\\Dropbox\\2017-2018Master2\\Techno_Web\\TP\\InteliJ\\BeautyOne\\src\\output\\outPutMoi.xml";
+
+
+
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+
+}
